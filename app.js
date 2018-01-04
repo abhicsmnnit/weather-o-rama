d00f5e0c06d5f18022b28fe9aba74a257c054bd2
Abhinav Verma <abhicsmnnit@gmail.com>
2018-01-04 18:47:27 +0530

    Pulled out the geocode related code into a separate module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode');
 
 const argv = yargs
                 .options({
@@ -13,26 +14,11 @@ const argv = yargs
                 .help()
                 .argv;
 
-const address = argv.address;
-const encodedAddress = encodeURIComponent(address);
-
-request({
-
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-
-}, (error, response, body) => {
-    
+geocode.geocodeAddress(argv.address, (error, results) => {
     if (error) {
-        console.log('Unable to connect to Google Servers.');
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Unable to find that address.');
-    } else if (body.status === 'OK') {
-        console.log(`Address: ${body.results[0].formatted_address}`);
-        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-    } else {
-        console.log('Unexpected error occurred.');
+        console.log(error);
     }
-
+    else {
+        console.log(JSON.stringify(results, undefined, 2));
+    }
 });
---
+++ geocode/index.js
@@ -0,0 +1,31 @@
+const request = require('request');
+
+const geocodeAddress = (address, callback) => {
+    const encodedAddress = encodeURIComponent(address);
+
+    request({
+    
+        url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+        json: true
+    
+    }, (error, response, body) => {
+        
+        if (error) {
+            callback('Unable to connect to Google Servers.');
+        } else if (body.status === 'ZERO_RESULTS') {
+            callback('Unable to find that address.');
+        } else if (body.status === 'OK') {
+            const results = {
+                address: body.results[0].formatted_address,
+                latitude: body.results[0].geometry.location.lat,
+                longitude: body.results[0].geometry.location.lng
+            };
+            callback(undefined, results);
+        } else {
+            callback('Unexpected error occurred.');
+        }
+    
+    });
+};
+
+module.exports.geocodeAddress = geocodeAddress;
